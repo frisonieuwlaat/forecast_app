94d1645b52a667b5c4d05216a8c7fd53edf0d18b
frisonieuwlaat <frisonieuwlaat@gmail.com>
2016-08-23 16:30:27 +0200

    Add files via upload

--- public/services.js
+++ public/services.js
@@ -8,7 +8,7 @@ weatherApp.service('cityService', function() {
 weatherApp.service('weatherService', ['$resource', function($resource) {
    
     this.GetWeather = function(city, days) {
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?q=zwolle&mode=json&units=metric&appid=ae75fb0d24dc16f7b4d367aeff6eda9d", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+        var weatherAPI = $resource("//api.openweathermap.org/data/2.5/forecast/daily?q=zwolle&mode=json&units=metric&appid=ae75fb0d24dc16f7b4d367aeff6eda9d", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
         return weatherResult = weatherAPI.get({ q: city, cnt: days });
     };
@@ -18,7 +18,7 @@ weatherApp.service('weatherService', ['$resource', function($resource) {
 weatherApp.service('weatherService2', ['$resource', function($resource) {
    
     this.GetWeather = function(city, days) {
-        var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/?q=rotterdam&mode=json&units=metric&appid=ae75fb0d24dc16f7b4d367aeff6eda9d", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+        var weatherAPI = $resource("//api.openweathermap.org/data/2.5/forecast/?q=rotterdam&mode=json&units=metric&appid=ae75fb0d24dc16f7b4d367aeff6eda9d", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
         return weatherResult2 = weatherAPI.get({ q: city, cnt: days });
     };
